7ecd0aba2c1cd8451d67e5b0136893035e76ac7b
ibrahim badr <ibrahimbdr@outlook.com>
2023-03-31 23:34:28 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { PostObj } from '../models/post-obj';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  postsArr: PostObj[] = [];
+  constructor() {
+    this.postsArr = [
+      {id:'1', post: "Here's a small text description for the card content. Nothing more, nothing less."},
+      {id:'2', post: "Here's a small text description for the card content. Nothing more, nothing less."},
+      {id:'3', post: "Here's a small text description for the card content. Nothing more, nothing less."},
+      {id:'4', post: "Here's a small text description for the card content. Nothing more, nothing less."},
+      {id:'5', post: "Here's a small text description for the card content. Nothing more, nothing less."},
+    ]
+
+  }
+
+  likePost(id: any){
+
+  }
+
+  commentPost(id: any){
+
+  }
+
+  editPost(id: any){
+
+  }
+
 }
